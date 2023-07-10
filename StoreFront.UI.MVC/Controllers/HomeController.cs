77df3e35ad25c65a181287dd10a38fa07ec6fb90
Andrew Cocroft <drew.cocroft@outlook.com>
2023-07-10 00:01:24 -0500

    Started out with styling, and then moved on to working on the contact form as well as starting the cart functionality. Will hopefully have both done tomorrow with some reminders and help.

--- StoreFront.UI.MVC/Controllers/HomeController.cs
+++ StoreFront.UI.MVC/Controllers/HomeController.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using StoreFront.UI.MVC.Models;
 using System.Diagnostics;
+using System.Net.Mail;
+using MimeKit;
+using MailKit;
 
 namespace StoreFront.UI.MVC.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _config;
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -26,6 +30,66 @@ namespace StoreFront.UI.MVC.Controllers
         public IActionResult ShopDetail()
         {
             return View();
+        }
+
+        public IActionResult GetContact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel cvm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(cvm);
+            }
+
+            string message = $"You have received a new email from your site's contact form!<br/>" +
+                $"Sender: {cvm.Name}<br/>" +
+                $"Email: {cvm.Email}<br/>" +
+                $"Subject: {cvm.Subject}<br/>" +
+                $"Message: {cvm.Message}<br/>";
+
+            var mm = new MimeMessage();
+
+            mm.From.Add(new MailboxAddress("Sender", _config.GetValue<string>("Credentials:Emails:User")));
+
+
+            mm.To.Add(new MailboxAddress("Personal", _config.GetValue<string>("Credentials:Emails:Recipient")));
+
+            mm.Subject = cvm.Subject;
+
+            mm.Body = new TextPart("HTML") { Text = message };
+
+            mm.Priority = MessagePriority.Urgent;
+
+            mm.ReplyTo.Add(new MailboxAddress("User", cvm.Email));
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect(_config.GetValue<string>("Credentials:Emails:Client"));
+
+                client.Authenticate(
+
+                    _config.GetValue<string>("Credentials:Emails:User"),
+
+                    _config.GetValue<string>("Credentials:Emails:Password")
+
+                    );
+                try
+                {
+                    client.Send(mm);
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.ErrorMessage = $"There was an error processing your request. Please " +
+                        $"try again later.<br/>Error Message: {ex.StackTrace}";
+
+                    return View(cvm);
+                }
+            }
+            return View("EmailConfirmation", cvm);
         }
 
         public IActionResult Privacy()
---
+++ StoreFront.UI.MVC/Controllers/ShoppingCartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StoreFront.UI.MVC.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
